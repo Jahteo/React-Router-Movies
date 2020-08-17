3abd4e032c993ceae210b8c6fec93526ca7d4450
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-17 14:52:51 -0700

    getting data, but switch routes in app.js commented out be the data isn't being passed down correctly yet , somehow.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link, Switch, Route } from "react-router-dom"
 
+import MovieList from "./Movies/MovieList"
+import Movie from "./Movies/Movie"
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -13,6 +16,7 @@ const App = () => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovieList(response.data);
+          console.log(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -25,9 +29,18 @@ const App = () => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
+  console.log(movieList)
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+      {/* <Switch>
+        <Route path="/movies/:id">
+          <Movie />
+        </Route>
+        <Route path="/">
+          <MovieList movieList={movieList}/>
+        </Route>
+      </Switch> */}
       <div>Replace this Div with your Routes</div>
     </div>
   );
