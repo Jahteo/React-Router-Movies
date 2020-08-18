c5be5ea37be41f837cda558b8821d73bcc199752
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-18 13:46:21 -0700

    tried to replicate how useHistory was used in our lecture project, didn't succeed.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useRouteMatch} from "react-router-dom"
+import { Link, useRouteMatch, useHistory } from "react-router-dom"
 
 const MovieList = ({ movies }) => {
   return (
@@ -14,9 +14,18 @@ const MovieList = ({ movies }) => {
 function MovieDetails({ movie }) {
   const { title, director, metascore } = movie;
   const { url } = useRouteMatch();
+
+  const history = useHistory()
+  const routeToCard = () => {
+    history.push("/movie")
+  }
+
   return (
     <div className="movie-card">
-      <Link to={`${url}/${movie.id}`}>
+      <Link
+        to={`${url}/${movie.id}`}
+        onClick={routeToCard}
+      >
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
